578d1e3486fb191064bfa65957bc2cf9bf0883a5
Ellen Arteca <ellen.arteca@gmail.com>
2020-03-19 10:59:19 -0400

    accidentally overwrote files in an old commit

--- tools/fetch-releases.js
+++ tools/fetch-releases.js
@@ -1,89 +1,50 @@
-
-
 const path = require('path')
-
 const fs = require('fs-extra')
-
 const https = require('https')
-
 
 const file = path.join(__dirname, '..', 'static', 'releases.json')
 
-
-function getReleases() 
-{
-  
-return new Promise((resolve) => 
-{
-    
-https.get({
+function getReleases() {
+  return new Promise((resolve) => {
+    https.get({
       host: 'registry.npmjs.org',
       path: '/electron',
       headers: {
         'User-Agent': 'Electron Fiddle'
       }
-    }, res => 
-{
-      
-res.setEncoding('utf8')
-      
-let body = ''
+    }, res => {
+      res.setEncoding('utf8')
+      let body = ''
 
-      
-res.on('data', data => 
-{
-        
-body += data;
+      res.on('data', data => {
+        body += data;
       })
 
-
-      
-res.on('end', () => 
-{
-        
-resolve(JSON.parse(body))
+      res.on('end', () => {
+        resolve(JSON.parse(body))
       })
-
     })
-
   })
-
 }
 
-
-async function main() 
-{
-  
-const data = await getReleases()
-  
-const releases = Object.keys(data.versions)
+async function main() {
+  const data = await getReleases()
+  const releases = Object.keys(data.versions)
     .map((version) => ({ version }))
 
-  
-console.log(`Updating local releases.json with ${releases.length} versions.`)
+  console.log(`Updating local releases.json with ${releases.length} versions.`)
 
-  
-console.error("TEMP_VAR_AUTOGEN_CALLING_2__RANDOM");
- 
-await fs.remove(file)
-  
-console.error("TEMP_VAR_AUTOGEN_CALLING_3__RANDOM");
- 
-await fs.outputFile(file, JSON.stringify(releases))
+  await fs.remove(file)
+  await fs.outputFile(file, JSON.stringify(releases))
 
-  
-console.log('Updating tests with new expected version count.')
+  console.log('Updating tests with new expected version count.')
 
-  
-const metadata = {
+  const metadata = {
     expectedVersionCount: releases.length,
     lastElectronVersion: releases[releases.length - 1].version
   }
-  
-const releasesMetadataPath = path.resolve(__dirname, '..', 'tests', 'fixtures', 'releases-metadata.json')
-  
-await fs.writeJson(releasesMetadataPath, metadata, { spaces: 2 })
+  const releasesMetadataPath = path.resolve(__dirname, '..', 'tests', 'fixtures', 'releases-metadata.json')
+  await fs.writeJson(releasesMetadataPath, metadata, { spaces: 2 })
 }
-
 
 main()
